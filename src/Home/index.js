2f6d6266e67b5b4ad5522341ba1a35aaa0fbb84e
Luciene <lucienebsousa@gmail.com>
2021-04-11 12:12:56 -0300

    card 1 from home

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,54 +1,61 @@
 import React from "react";
-import { Card, Figure, FigureImage } from "react-bootstrap";
+import { Card, Figure } from "react-bootstrap";
 import ButtonCard from "./../components/buttonCard";
 import "./home.css";
 
+import cel1 from "./../assets/cel1.svg";
+import cel2 from "./../assets/cel2.svg";
 import cel3 from "./../assets/cel3.svg";
 import cel4 from "./../assets/cel4.svg";
-import cel5 from "./../assets/cel5.svg";
+import FigureImage from "react-bootstrap/esm/FigureImage";
 
 function Home() {
   return (
     <div>
-      <Card className="fundo">
-        <Card.Body>
-          <Card.Text className="text-title">Food app</Card.Text>
-          <Card.Text className="title">Why stay hungry when</Card.Text>
-          <Card.Text className="title">
-            you can order form Bella Onojie
-          </Card.Text>
-          <Card.Text className="subtitle">
-            Download the bella onoje’s food app now on
-          </Card.Text>
-          <div className="card-button">
-            <ButtonCard class="btn-play" text="Playstore"></ButtonCard>
-            <ButtonCard class="btn-app" text="App store"></ButtonCard>
-          </div>
+      <Card bsPrefix="fundo">
+        <Card.Text className="text-title">Food app</Card.Text>
+        <Card.Text className="title">Why stay hungry when</Card.Text>
+        <Card.Text className="title">you can order form Bella Onojie</Card.Text>
+        <Card.Text className="subtitle">
+          Download the bella onoje’s food app now on
+        </Card.Text>
+        <div className="card-button">
+          <ButtonCard class="btn-play" text="Playstore"></ButtonCard>
+          <ButtonCard class="btn-app" text="App store"></ButtonCard>
+        </div>
+      </Card>
+
+      <Card>
+        <Card.Text bsPrefix="card-wrapper-duplo-celular">
+          <Figure>
+            <Figure.Image bsPrefix="card-fig" src={cel1} />
+          </Figure>
+          <Card.Body bsPrefix="card-wrapper-app">
+            <Card.Title bsPrefix="line"></Card.Title>
+            <Card.Text bsPrefix="card-title-app">How the app works</Card.Text>
+          </Card.Body>
+        </Card.Text>
+      </Card>
+
+      <Card bsPrefix="card">
+        <Card.Body bsPrefix="card-wrapper">
+          <Figure bsPrefix="card-wrapper-cel">
+            <FigureImage src={cel2} bsPrefix="img-cel" />
+          </Figure>
+          <Card.Body bsPrefix="card-wrapper-text">
+            <Card.Text bsPrefix="txt-create">Create an account</Card.Text>
+            <Card.Text bsPrefix="title-create">
+              Create/login to an existing account to get started
+            </Card.Text>
+            <Card.Text bsPrefix="subtitle-account">
+              An account is created with your email and a desired password
+            </Card.Text>
+          </Card.Body>
         </Card.Body>
       </Card>
-      <div>
-        <Card>
-          <Card.Text className="card-title">How the app works</Card.Text>
-        </Card>
-      </div>
-
+      {/*
       <div class="card-wrapper">
-        <div class="card-cel">
-          <img src={cel3} className="img-cel" />
-        </div>
-        <div class="card-text">
-          <h3 className="txt-create">Create an account</h3>
-          <h2 className="title-create">
-            Create/login to an existing account to get started
-          </h2>
-          <h4 className="subtitle-account">
-            An account is created with your email and a desired password
-          </h4>
-        </div>
-      </div>
-
-      <div class="card-wrapper">
-        <div class="card-text">
+        <div class="card-wrapper-text">
           <h3 className="txt-create">Explore varieties</h3>
           <h2 className="title-create">
             Shop for your favorites meal as e dey hot.
@@ -57,18 +64,18 @@ function Home() {
             Shop for your favorite meals or drinks and enjoy while doing it.
           </h4>
         </div>
-        <div class="card-cel">
-          <img src={cel4} className="img-cel" />
+        <div class="card-wrapper-cel">
+          <img src={cel3} className="img-cel" />
         </div>
       </div>
 
       <div class="card-wrapper">
-        <div class="card-cel">
-          <Figure>
-            <Figure.Image width={278} height={511} src={cel5} />
-          </Figure>
+        <div class="card-wrapper-cel">
+          <div>
+            <img src={cel4} />
+          </div>
         </div>
-        <div class="card-text">
+        <div class="card-wrapper-text">
           <h3 className="txt-create">Checkout</h3>
           <h2 className="title-create">
             When you done check out and get it delivered.
@@ -77,7 +84,7 @@ function Home() {
             When you done check out and get it delivered with ease.
           </h4>
         </div>
-      </div>
+      </div> */}
 
       <div>
         <Card className="segundo-fundo session-web">
